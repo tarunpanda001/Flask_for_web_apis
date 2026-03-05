add18aaa2f5036cfe19120dd3f4aa3d4fbb1d8a8
Tarun Panda <tarunpanda2005@gmail.com>
2026-03-05 18:52:58 +0530

    intial flask code

---
+++ hello_flask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)  # __name__ -> "flask.py"
+
+@app.route("/") # endpoint When user visits "/", this function will  automatically be called
+def home():
+    return "This is the Home Page"
+
+@app.route("/about")
+def about():
+    return "This is page about us"
+
+if name =="__main__":
+    app.run(debug=True) # debug=True -> auto reload the server when code changes
